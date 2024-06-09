6165b298328d8677650aab68b3a43742a4c9dee4
Tiago Silva <tlontro@gmail.com>
2024-06-09 06:39:32 -0300

    MODAK-7: Adding wait_until + moving to next page

--- features/pages/aliexpress_catalog_page.rb
+++ features/pages/aliexpress_catalog_page.rb
@@ -3,4 +3,5 @@ class AliExpressCatalogPage < SitePrism::Page
   set_url "/"
   elements :pagination, ".comet-pagination-item"
   element :active_page_number, ".comet-pagination-item-active"
+  elements :products, "#card-list [class*='list--gallery']"
 end
--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -11,6 +11,22 @@ module Helpers
   rescue Capybara::ElementNotFound
     puts "no notifications popup"
   end
+
+  def wait_until(time: 60, sleep_time: 0.5, msg: "Condition was not achieved in time!", should_break: true)
+    raise "No block given!" unless block_given?
+
+    retries = 0
+    begin
+        sleep(sleep_time)
+        yield ? (return true) : (raise "Condition still false!")
+    rescue RuntimeError
+        retries += 1
+        retry if retries < (time / sleep_time)
+    end
+
+    should_break ? (raise msg) : (return false)
+  end
+
 end
 
 World(Helpers)
